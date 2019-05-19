cadf2a6866349af5ca1b39e2bd0dd7e9b0beb364
Schiver <enggarswahadika@gmail.com>
2019-05-19 11:47:29 +0700

    Adding link

--- app/src/main/java/com/example/schiver/sethings/LoginActivity.java
+++ app/src/main/java/com/example/schiver/sethings/LoginActivity.java
@@ -2,14 +2,25 @@ package com.example.schiver.sethings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView myColoredTextView;
+
+    private String getColoredSpanned(String text , String color){
+        String inputText = "<font color=\" + color + \">\" + text + \"</font>";
+        return inputText;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // test again
-        
+
+        myColoredTextView = (TextView) findViewById(R.id.sign_upText);
+        String name = getColoredSpanned("New user?", "#800000");
+        String surName = getColoredSpanned("Sign up here","#000080");
+
+
     }
 }
