2816a2c5f4f459316b92ca52d44ba865f19ea9a6
Schiver <enggarswahadika@gmail.com>
2019-05-25 12:27:06 +0700

    Adding bottom navbar and basic operation

--- app/src/main/java/com/example/schiver/sethings/MainActivity.java
+++ app/src/main/java/com/example/schiver/sethings/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.schiver.sethings;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,17 +20,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logout = (Button) findViewById(R.id.button);
-        logout.setOnClickListener(new View.OnClickListener() {
+       // logout = (Button) findViewById(R.id.button);
+        /*logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SharedPref.saveSharefPref(getApplicationContext(),"session","false");
                 Intent walkThrougIntent = new Intent(getApplicationContext(),WalkthroughActivity.class);
                 startActivity(walkThrougIntent);
             }
-        });
+        });*/
+
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
         mySession();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.nav_home :
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_devices :
+                    selectedFragment = new DevicesFragment();
+                    break;
+                case R.id.nav_config :
+                    selectedFragment = new ConfigFragment();
+                    break;
+                case R.id.nav_usage :
+                    selectedFragment = new UsageFragment();
+                    break;
+                case R.id.nav_help :
+                    selectedFragment = new HelpFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container,selectedFragment)
+                    .commit();
+            return true;
+        }
+    };
 
     public void mySession(){
         session = Boolean.valueOf(SharedPref.readSharedPref(getApplicationContext(),"session","false"));
